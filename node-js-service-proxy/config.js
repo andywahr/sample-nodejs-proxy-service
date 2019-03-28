ed585e7938450e345b084c8df1b05c861e29c1c4
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-28 16:25:25 -0500

    fixed configuration

--- node-js-service-proxy/config.js
+++ node-js-service-proxy/config.js
@@ -1,9 +1,9 @@
 
 
 module.exports = {
-    cosmosAccountName : "",
-    cosmosAccountPassword : "",
-    storageAccountName: "",
-    storageAccountKey: "",
-    keyVaultAccountName: ""
+    cosmosAccountName : process.env.cosmosAccountName,
+    cosmosAccountPassword : process.env.cosmosAccountPassword,
+    storageAccountName: process.env.storageAccountName,
+    storageAccountKey: process.env.storageAccountKey,
+    keyVaultAccountName: process.env.keyVaultAccountName
 };
