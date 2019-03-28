adb8d0aa00285bb87ffd7945d20c783a3def2ba5
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-28 16:18:27 -0500

    Update config.js

--- node-js-service-proxy/config.js
+++ node-js-service-proxy/config.js
@@ -1,9 +1,9 @@
 
 
 module.exports = {
-    cosmosAccountName : "andywahrfairtest",
-    cosmosAccountPassword : "CKUVbt66NmwT5ydymQURq9X0Ik7d1bfqJYQ8PRuFBhCmvTUDeF38xiyoXCNkOjPi8mRwc05h6GaGKcxT0zG8Hw==",
-    storageAccountName: "andywahrfair1",
-    storageAccountKey: "SusLfi5IfXGTSYzrEOkYDZkPiwTeZImF02V04uIwbiV9TLwDb2X+oaAX6UWHBW87DP6qtIiP2PPxyDRw6m9VdA==",
-    keyVaultAccountName: "kvandywahrfair"
+    cosmosAccountName : "",
+    cosmosAccountPassword : "",
+    storageAccountName: "",
+    storageAccountKey: "",
+    keyVaultAccountName: ""
 };
